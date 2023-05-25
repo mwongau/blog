008204236098903300f6927e93a0495e400132c1
M Wong <wongmt15@gmail.com>
2023-05-26 09:25:00 +1000

    26-5-23

--- src/app/page.js
+++ src/app/page.js
@@ -9,16 +9,16 @@ export default function Page() {
     <main className={styles.main}>
       
       <div>
-      <h1> M. Wong</h1>
+      <h1> M Wong</h1>
 	  <br />
       <h2>Home</h2>	  
 	  
-	  <p>Welcome to personal site of M Wong. I am a researcher in computer vision and machine learning 
+	  <p>Welcome to my personal site. I am a researcher in computer vision and machine learning 
 	  with Ph.D in Computer Science, M.Sc and B.Sc(Hons.) in Electronic Engineering. I am also 
 	  interested in web app development. This site is built by using Next.js, React, JavaScript, 
 	  HTML and CSS. This site is hosted by GitHub Pages.</p>
 	  <p>This site might use cookies to improve the browsing experience of users. If you 
-	  continue to use this site, it is assumed that you agree to the use of cookies.</p> 
+	  continue to use this site, it will be assumed that you agree to the use of cookies.</p> 
 	  <br /><br />
 	  <Link href="/posts"><b>Go to Blog page</b></Link>
       
