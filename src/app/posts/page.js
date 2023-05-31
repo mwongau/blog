40d708e6baddd89216515394d11df44c23f756cb
M Wong <wongmt15@gmail.com>
2023-06-01 07:23:06 +1000

    1-6

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -5,9 +5,9 @@ export default function Page() {
     <>
       <p><Link href="/home"> Home </Link> |
 	  <Link href="/posts"> Posts selection </Link></p>  	
-	  <h2>Blog</h2>
+	  <h2>Blog of M Wong</h2>
       
-      <p>Select and click post title:</p>
+	  <p>Select and click post title:</p>
 	  <p><Link href="/posts/post1">Creation of blog</Link></p>
       <p><Link href="/posts/post2">About this site</Link></p>
 	 
